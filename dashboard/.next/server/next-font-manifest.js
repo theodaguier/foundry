9647dfa4f27691da596f2e18e7ab6a8bc8ac7ea0
Théo Daguier <theo.daguier@icloud.com>
2026-03-27 11:07:03 +0100

    chore(dashboard): remove .next build artifacts from tracking

--- dashboard/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/theodaguier/Developer/personal/foundry/dashboard/app/layout\":[\"static/media/93f479601ee12b01-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
